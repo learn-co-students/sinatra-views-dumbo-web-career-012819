d7b0653b27221ad0ff3b26f5aca66aa903dcf091
github username <github email address>
2019-02-20 15:35:38 -0500

    Done.

--- app.rb
+++ app.rb
@@ -3,6 +3,15 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
 	get '/' do
-		
+		erb :index
+	end
+
+	get "/info" do
+	  "Testing the info page"
+		erb :info
+	end
+
+	get "/info" do
+	  erb :dogs
 	end
 end
